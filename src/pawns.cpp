dcfd6a05513d68d80d83fb0b509e1b2a392d6523
David Zar <zardav@gmail.com>
2014-06-18 15:19:56 +0300

    Decrease bonus by min KP distance

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -316,7 +316,7 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
       bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
 
-  return make_score(bonus, -16 * minKPdistance[Us]);
+  return make_score(bonus - minKPdistance[Us], -16 * minKPdistance[Us]);
 }
 
 // Explicit template instantiation
