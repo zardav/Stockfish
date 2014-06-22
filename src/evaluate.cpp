bf6ec6cc17088572ec9527d0997f35fab4b46ebb
David Zar <zardav@gmail.com>
2014-06-23 00:15:58 +0300

    Change the base bonus Bench: 8426445

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -548,7 +548,7 @@ namespace {
         int rr = r * (r - 1);
 
         // Base bonus based on rank
-        Value mbonus = Value(rr * 16 + r * 2), ebonus = Value(7 * (rr + r + 1));
+        Value mbonus = Value(rr * 18), ebonus = Value(7 * (rr + r + 1) + rr);
 
         if (rr)
         {
